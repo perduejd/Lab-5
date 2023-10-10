7e097e8ab4677f5376096ad9cb84bff185fc24b3
Jacob Perdue <113634959+perduejd@users.noreply.github.com>
2023-10-10 16:33:06 -0400

    Add files via upload

---
+++ JokeAPIService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using DadJokeAPI;
+
+namespace DadJokeAPI
+{
+    public class JokeApiService
+    {
+        private readonly HttpClient _client; // Http Client is a class that allows us to make HTTP requests
+
+        public JokeApiService() // Initalize HttpClient and set the Accept header to application/json
+        {
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add("Accept", "application/json"); // Setting the Accept header to application/json
+        }
+
+        public async Task<string> GetRandomJokeAsync() // Method used to retrieve a random joke from the API asynchonously
+        {
+            HttpResponseMessage response = await _client.GetAsync("https://icanhazdadjoke.com/"); // GET req to random joke endpoint
+            response.EnsureSuccessStatusCode(); // Ensuring the respone is successful
+            string responseBody = await response.Content.ReadAsStringAsync();
+            dynamic jokeObject = JsonConvert.DeserializeObject(responseBody); // Deserialize JSON response to dynamic object
+            return jokeObject.joke; // Returns the random joke
+        }
+
+        public async Task<string[]> SearchJokesAsync(string term) // Method that allows you to search for a dad joke based on a search term asynchonously
+        {
+            HttpResponseMessage response = await _client.GetAsync($"https://icanhazdadjoke.com/search?term={term}"); // GET request to search endpoint with provided search term
+            response.EnsureSuccessStatusCode(); 
+            string responseBody = await response.Content.ReadAsStringAsync(); 
+            dynamic jokesResponse = JsonConvert.DeserializeObject(responseBody); 
+
+            List<string> jokesList = new List<string>(); // Creates a list to store retrieved jokes
+
+            foreach (var jokeObject in jokesResponse.results) // Iterates through results and add jokes to the list 
+            {
+                jokesList.Add(jokeObject.joke.ToString());
+            }
+
+            return jokesList.ToArray(); // Returns the list of jokes as an array
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using DadJokeAPI;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        JokeApiService jokeApiService = new JokeApiService(); // Creates a new instance of the JokeApiService class
+
+        while (true) // Menu
+        {
+            Console.WriteLine("Choose an option:\n");
+            Console.WriteLine("1. Get a terrible Dad Joke");
+            Console.WriteLine("2. Search for Dad Jokes by Key Word");
+            Console.WriteLine("3. Exit Dad Joke App");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    try
+                    {
+                        string randomJoke = await jokeApiService.GetRandomJokeAsync(); // Calls the GetRandomJokeAsync method
+                        Console.WriteLine($"\nRandom Joke: {randomJoke}\n"); // Displays the random joke
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Enter search term: "); // Prompts user to enter a search term
+                    string searchTerm = Console.ReadLine(); // Stores the search term in a variable
+                    try
+                    {
+                        var jokes = await jokeApiService.SearchJokesAsync(searchTerm); // Calls the SearchJokesAsync method
+                        foreach (var joke in jokes) // Iterates through the jokes and displays them
+                        {
+                            Console.WriteLine(joke); // Displays the joke
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    break;
+
+                case "3":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
